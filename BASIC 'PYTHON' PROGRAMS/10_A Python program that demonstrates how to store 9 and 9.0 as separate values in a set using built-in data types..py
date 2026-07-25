464d4f384ed4f9c35ae5ce6e648a9b70a51902d8
ProjectsWithSuraj <surajwithprojects@gmail.com>
2026-07-25 20:46:58 +0545

    Added another simple code

---
+++ BASIC 'PYTHON' PROGRAMS/10_A Python program that demonstrates how to store 9 and 9.0 as separate values in a set using built-in data types..py
@@ -0,0 +1,14 @@
+"""There are two ways...
+1st Way"""
+
+values={
+    ("int", 9),
+    ("float", 9.0)
+}
+print(values)
+
+#2nd Way
+
+value={9,"9.0"}   #Here, 9 is showed as int and 9.0 is showed as string.
+print(value)
+
