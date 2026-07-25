7334afa93252a093b041a238ad8645301d255a33
ProjectsWithSuraj <surajwithprojects@gmail.com>
2026-07-25 19:37:26 +0545

    added comment

--- BASIC 'PYTHON' PROGRAMS/07_A simple Python program that stores a set of word meanings using a dictionary data structure..py
+++ BASIC 'PYTHON' PROGRAMS/07_A simple Python program that stores a set of word meanings using a dictionary data structure..py
@@ -1,3 +1,6 @@
+#table:"a piece of furniture","list of facts and figures"
+#cat:"a small animal"
+
 dict={
     "table":["a piece of furniture", "list of facts and figures"],
     "cat":"a small animal"
