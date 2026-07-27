c202a80bf455284d83d0bcdbbbcf3641191c3d77
ProjectsWithSuraj <surajwithprojects@gmail.com>
2026-07-27 17:25:24 +0545

    Added more useful codes for brginners to learn python.

---
+++ BASIC 'PYTHON' PROGRAMS/21_➕ Sum of First N Numbers(while loop).py
@@ -0,0 +1,7 @@
+num=5
+sum=0
+i=1
+while i<=num:
+    sum+=i
+    i+=1
+print("Sum of numbers: ", sum)
---
+++ BASIC 'PYTHON' PROGRAMS/22_➕ Sum of Factorials of 1st N numbers(for loop).py
@@ -0,0 +1,7 @@
+num=3
+fact=1
+i=1
+for i in range(1,num+1):
+    fact*=i
+    i+=1
+print("Factorial: ", fact)
